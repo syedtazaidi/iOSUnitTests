1ba4d8882e869a8145ed2ba7e8e3b5e60a3657c7
Taha <tahauwaterloo@outlook.com>
2022-04-26 12:34:10 -0400

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Taha Ali on 2022-04-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
